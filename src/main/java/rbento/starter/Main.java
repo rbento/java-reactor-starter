feb27458fa5e9b8d7fc921daf28165f8de383d97
Rodrigo Bento <rodrigo.bento@gmail.com>
2023-08-02 02:23:14 -0400

    Initial commit

---
+++ src/main/java/rbento/starter/Main.java
@@ -0,0 +1,13 @@
+
+/* Copyright (c) 2023 Rodrigo Bento */
+
+package rbento.starter;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("Running...");
+    }
+}
---
+++ src/test/java/rbento/starter/ReactorTest.java
@@ -0,0 +1,11 @@
+
+/* Copyright (c) 2023 Rodrigo Bento */
+
+package rbento.starter;
+
+import org.junit.jupiter.api.*;
+
+class ReactorTest {
+    @Test
+    public void testReactor() {}
+}
